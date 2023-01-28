731465c19f52e75771198b85d0a33288acb13d07
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-01-28 08:03:15 +0200

    use global validation pipe for dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true, // prevent sqli and xss in class-validator@0.13.2
+      disableErrorMessages: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
