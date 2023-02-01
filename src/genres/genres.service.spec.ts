5c83b71b52de3c2ed2e59ed3df3c561ccb8f108b
Vladyslav Vyslobitskyi <45492416+Mole-Miner@users.noreply.github.com>
2023-02-01 17:42:32 +0200

    test genres service

--- src/genres/genres.service.spec.ts
+++ src/genres/genres.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
 import { GenresService } from './genres.service';
+import { Genre } from './entities/genre.entity';
 
 describe('GenresService', () => {
   let service: GenresService;
 
+  const mockGenresRepository = {
+    create: jest.fn().mockImplementation((dto: any) => dto),
+    save: jest
+      .fn()
+      .mockImplementation((dto: any) =>
+        Promise.resolve({ id: Date.now(), ...dto }),
+      ),
+    find: jest.fn().mockResolvedValue([{ id: Date.now(), name: 'genre a' }]),
+    findOneBy: jest
+      .fn()
+      .mockImplementation(({ id }: { id: number }) =>
+        Promise.resolve({ id, name: 'genre a' }),
+      ),
+    update: jest
+      .fn()
+      .mockImplementation((id: number, dto: any) =>
+        Promise.resolve({ id, ...dto }),
+      ),
+    remove: jest
+      .fn()
+      .mockImplementation(({ id }: { id: number }) =>
+        Promise.resolve({ id, name: 'genre a' }),
+      ),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GenresService],
+      providers: [
+        GenresService,
+        { provide: getRepositoryToken(Genre), useValue: mockGenresRepository },
+      ],
     }).compile();
 
     service = module.get<GenresService>(GenresService);
@@ -14,5 +45,34 @@ describe('GenresService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create genre', async () => {
+    expect(await service.create({ name: 'genre a' })).toEqual({
+      id: expect.any(Number),
+      name: 'genre a',
+    });
+  });
+
+  it('should find all genres', async () => {
+    expect(await service.findAll()).toEqual([
+      { id: expect.any(Number), name: 'genre a' },
+    ]);
+  });
+
+  it('should find one genre by id', async () => {
+    const id = 1;
+    expect(await service.findOne(id)).toEqual({ id, name: 'genre a' });
+  });
+
+  it('should find one genre by id and update it', async () => {
+    const id = 1;
+    const payload = { name: 'genre a' };
+    expect(await service.update(id, payload)).toEqual({ id, ...payload });
+  });
+
+  it('should find one genre by id and remove it', async () => {
+    const id = 1;
+    expect(await service.remove(id)).toEqual({ id, name: 'genre a' });
   });
 });
